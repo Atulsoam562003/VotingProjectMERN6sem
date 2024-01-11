e0103a2580210b3db30a36ba01ebf56fad29d95b
Atulsoam562003 <atulsoam5@gmail.com>
2024-01-11 14:49:22 +0530

    Set up api service

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React , {Component} from 'react';
+import api from '../services/api';
 
-const App = () => {
-    return (
-        <div>App Works</div>
-    );
+class App extends Component {
+    async componentDidMount() {
+        const result = await api.call('post' , 'auth/login' , {
+            username : 'username' ,
+            password : 'password'
+        });
+
+        console.log(result);
+    }
+    render () {
+        return <div>App works!</div>;
+    }
 }
 
 export default App;
