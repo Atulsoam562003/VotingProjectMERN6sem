c8fb21727ad7fd482a94e24a94a4883bd453447c
Atulsoam562003 <atulsoam5@gmail.com>
2024-01-13 18:32:14 +0530

    Added Auth componenet

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -1,22 +1,25 @@
-import React , {Component} from 'react';
-import {Provider} from 'react-redux';
-import {jwtDecode} from 'jwt-decode';
+import React from "react";
+import { Provider } from "react-redux";
+import { jwtDecode } from "jwt-decode";
 
-import {store} from '../store';
-import { addError, setCurrentUser ,setToken} from '../store/actions';
+import { store } from "../store";
+import { addError, setCurrentUser, setToken } from "../store/actions";
+import Auth from "../components/Auth";
+import ErrorMessage from "../components/ErrorMessage";
 
 if (localStorage.jwtToken) {
-    setToken(localStorage.jwtToken);
-    try {
-        store.dispatch(setCurrentUser(jwtDecode(localStorage.jwtToken)));
-    }catch (err) {
-        store.dispatch(setCurrentUser({}));
-        store.dispatch(addError(err));
-    }
+  setToken(localStorage.jwtToken);
+  try {
+    store.dispatch(setCurrentUser(jwtDecode(localStorage.jwtToken)));
+  } catch (err) {
+    store.dispatch(setCurrentUser({}));
+    store.dispatch(addError(err));
+  }
 }
 const App = () => (
-    <Provider store={store}>
-        <div>App Works</div>
-    </Provider>
+  <Provider store={store}>
+    <Auth authType={"login"} />
+    <ErrorMessage />
+  </Provider>
 );
 export default App;
